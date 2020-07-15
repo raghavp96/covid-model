31de14c5a112a7e4a797a874b028782b6d8b3dfc
Raghavprasanna Rajagopalan <raghav@Raghavprasannas-MacBook-Pro.local>
2020-07-15 14:57:18 -0400

    Refactor to support per-state cleaning methods

---
+++ covid/clean_us.py
@@ -0,0 +1,122 @@
+import pandas as pd
+
+def process(data: pd.DataFrame, idx: pd.IndexSlice):
+    states = {
+        "US_MI" : process_US_MI,
+        "US_NJ" : process_US_NJ,
+        "US_CA" : process_US_CA,
+        "US_SC" : process_US_SC,
+        "US_OR" : process_US_OR,
+        "US_OH" : process_US_OH,
+        "US_NV" : process_US_NV,
+        "US_WA" : process_US_WA,
+        "US_AL" : process_US_AL,
+        "US_AR" : process_US_AR,
+        "US_MS" : process_US_MS,
+        "US_PA" : process_US_PA
+    }
+
+    for processor in states.values():
+        data = processor(data, idx)
+
+    return data
+
+
+def process_US_MI(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Michigan missed 6/18 totals and lumped them into 6/19 so we've
+    # divided the totals in two and equally distributed to both days.
+    data.loc[idx["MI", pd.Timestamp("2020-06-18")], "total"] = 14871
+    data.loc[idx["MI", pd.Timestamp("2020-06-19")], "total"] = 14871
+
+    return data
+
+
+def process_US_NJ(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Huge outlier in NJ causing sampling issues.
+    data.loc[idx["NJ", pd.Timestamp("2020-05-11")], :] = 0
+
+    return data
+
+
+def process_US_CA(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Huge outlier in CA causing sampling issues.
+    data.loc[idx["CA", pd.Timestamp("2020-04-22")], :] = 0
+
+    return data
+
+
+def process_US_SC(data: pd.DataFrame, idx: pd.IndexSlice):
+    # TODO: generally should handle when # tests == # positives and that
+    # is not an indication of positive rate.
+    data.loc[idx["SC", pd.Timestamp("2020-06-26")], :] = 0
+
+    return data
+
+
+def process_US_OR(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Two days of no new data then lumped sum on third day with lack of new total tests
+    data.loc[idx["OR", pd.Timestamp("2020-06-26") : pd.Timestamp("2020-06-28")], 'positive'] = 174
+    data.loc[idx["OR", pd.Timestamp("2020-06-26") : pd.Timestamp("2020-06-28")], 'total'] = 3296
+
+    return data
+
+
+def process_US_OH(data: pd.DataFrame, idx: pd.IndexSlice):
+    # https://twitter.com/OHdeptofhealth/status/1278768987292209154
+    data.loc[idx["OH", pd.Timestamp("2020-07-01")], :] = 0
+    data.loc[idx["OH", pd.Timestamp("2020-07-09")], :] = 0
+
+    return data
+
+
+def process_US_NV(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Nevada didn't report total tests this day
+    data.loc[idx["NV", pd.Timestamp("2020-07-02")], :] = 0
+
+    return data
+
+
+def process_US_WA(data: pd.DataFrame, idx: pd.IndexSlice):
+    # A bunch of incorrect values for WA data so nulling them out.
+    data.loc[idx["WA", pd.Timestamp("2020-06-05") : pd.Timestamp("2020-06-07")], :] = 0
+    data.loc[idx["WA", pd.Timestamp("2020-06-20") : pd.Timestamp("2020-06-21")], :] = 0
+
+    return data
+
+
+def process_US_AL(data: pd.DataFrame, idx: pd.IndexSlice):
+    # AL reported tests == positives
+    data.loc[idx["AL", pd.Timestamp("2020-07-09")], :] = 0
+
+    return data
+
+
+def process_US_AR(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Low reported tests
+    data.loc[idx["AR", pd.Timestamp("2020-07-10")], :] = 0
+
+    return data
+
+
+def process_US_MS(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Positives == tests
+    data.loc[idx["MS", pd.Timestamp("2020-07-12")], :] = 0
+
+    return data
+
+
+def process_US_PA(data: pd.DataFrame, idx: pd.IndexSlice):
+    # Outlier dates in PA
+    data.loc[
+        idx[
+            "PA",
+            [
+                pd.Timestamp("2020-06-03"),
+                pd.Timestamp("2020-04-21"),
+                pd.Timestamp("2020-05-20"),
+            ],
+        ],
+        :,
+    ] = 0
+
+    return data
--- covid/data_us.py
+++ covid/data_us.py
@@ -5,6 +5,8 @@ import requests
 import pandas as pd
 import numpy as np
 
+from .clean_us import process as clean_states
+
 idx = pd.IndexSlice
 
 
@@ -38,62 +40,11 @@ def process_covidtracking_data(data: pd.DataFrame, run_date: pd.Timestamp):
     # Now work with daily counts
     data = data.diff().dropna().clip(0, None).sort_index()
 
-    # Michigan missed 6/18 totals and lumped them into 6/19 so we've
-    # divided the totals in two and equally distributed to both days.
-    data.loc[idx["MI", pd.Timestamp("2020-06-18")], "total"] = 14871
-    data.loc[idx["MI", pd.Timestamp("2020-06-19")], "total"] = 14871
-
     # Note that when we set total to zero, the model ignores that date. See
     # the likelihood function in GenerativeModel.build
 
-    # Huge outlier in NJ causing sampling issues.
-    data.loc[idx["NJ", pd.Timestamp("2020-05-11")], :] = 0
-
-    # Huge outlier in CA causing sampling issues.
-    data.loc[idx["CA", pd.Timestamp("2020-04-22")], :] = 0
-
-    # Huge outlier in CA causing sampling issues.
-    # TODO: generally should handle when # tests == # positives and that
-    # is not an indication of positive rate.
-    data.loc[idx["SC", pd.Timestamp("2020-06-26")], :] = 0
-
-    # Two days of no new data then lumped sum on third day with lack of new total tests
-    data.loc[idx["OR", pd.Timestamp("2020-06-26") : pd.Timestamp("2020-06-28")], 'positive'] = 174
-    data.loc[idx["OR", pd.Timestamp("2020-06-26") : pd.Timestamp("2020-06-28")], 'total'] = 3296
-
-
-    #https://twitter.com/OHdeptofhealth/status/1278768987292209154
-    data.loc[idx["OH", pd.Timestamp("2020-07-01")], :] = 0
-    data.loc[idx["OH", pd.Timestamp("2020-07-09")], :] = 0
-
-    # Nevada didn't report total tests this day
-    data.loc[idx["NV", pd.Timestamp("2020-07-02")], :] = 0
-
-    # A bunch of incorrect values for WA data so nulling them out.
-    data.loc[idx["WA", pd.Timestamp("2020-06-05") : pd.Timestamp("2020-06-07")], :] = 0
-    data.loc[idx["WA", pd.Timestamp("2020-06-20") : pd.Timestamp("2020-06-21")], :] = 0
-
-    # AL reported tests == positives
-    data.loc[idx["AL", pd.Timestamp("2020-07-09")], :] = 0
-
-    # Low reported tests
-    data.loc[idx["AR", pd.Timestamp("2020-07-10")], :] = 0
-
-    # Positives == tests
-    data.loc[idx["MS", pd.Timestamp("2020-07-12")], :] = 0
-
-    # Outlier dates in PA
-    data.loc[
-        idx[
-            "PA",
-            [
-                pd.Timestamp("2020-06-03"),
-                pd.Timestamp("2020-04-21"),
-                pd.Timestamp("2020-05-20"),
-            ],
-        ],
-        :,
-    ] = 0
+    # Clean data state-by-state
+    data = clean_states(data, idx)
 
     # At the real time of `run_date`, the data for `run_date` is not yet available!
     # Cutting it away is important for backtesting!
